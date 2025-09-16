94c6c6c343d0c0a1a45dad9db7d5c8bf10cf1969
kev405 <>
2025-09-16 16:59:36 -0500

    first commit

---
+++ domain/model/src/main/java/co/com/crediya/model/report/gateways/ReportRepository.java
@@ -0,0 +1,4 @@
+package co.com.crediya.model.report.gateways;
+
+public interface ReportRepository {
+}
---
+++ domain/usecase/src/main/java/co/com/crediya/usecase/report/ReportUseCase.java
@@ -0,0 +1,6 @@
+package co.com.crediya.usecase.report;
+
+import lombok.RequiredArgsConstructor;
+@RequiredArgsConstructor
+public class ReportUseCase {
+}
